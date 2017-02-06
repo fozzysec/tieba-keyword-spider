05f5e48b27359c4b1e8bcdcd5bef504f3698a46e
fozzy <fozzy@fozzy.co>
2017-02-07 02:21:13 +0800

    implement input script that use CRC32 digest

---
+++ input.pl
@@ -0,0 +1,17 @@
+#!/usr/bin/env perl
+#
+use Cwd qw(abs_path);
+use File::Basename;
+use Digest::CRC qw(crc32);
+use Encode qw(decode_utf8);
+
+use open ':std', ':encoding(UTF-8)';
+
+$maillist = 'maillist.conf';
+
+$path = dirname(abs_path(__FILE__)).'/';
+open(my $fh, '>>:encoding(UTF-8)', $path.$maillist) or die "failed open file";
+my @config = map{decode_utf8($_, 1)} @ARGV;
+$config[2] = crc32($config[0]).'.jl';
+print $fh join(':', @config)."\n";
+close($fh);

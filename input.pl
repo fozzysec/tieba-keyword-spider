f3d1a7dd4789c445ad2a2e681ad6dd9af8bed506
fozzy <fozzy@fozzy.co>
2017-02-07 10:33:58 +0800

    fix problem with crontab, abs_path is used on config.pl now

--- input.pl
+++ input.pl
@@ -7,11 +7,11 @@ use Encode qw(decode_utf8);
 
 use open ':std', ':encoding(UTF-8)';
 
-require "./config.pl";
+$path = dirname(abs_path(__FILE__)).'/';
+require $path."config.pl";
 
 my $maillist = $g_maillist;
 
-$path = dirname(abs_path(__FILE__)).'/';
 open(my $fh, '>>:encoding(UTF-8)', $path.$maillist) or die "failed open file";
 my @config = map{decode_utf8($_, 1)} @ARGV;
 $config[2] = crc32($config[0]).'.jl';

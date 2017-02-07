59af215254c50f4d843a811049a1776fd1674af5
fozzy <fozzy@fozzy.co>
2017-02-07 13:04:37 +0800

    implemented async spawn on spider

--- config.pl
+++ config.pl
@@ -2,3 +2,4 @@ $g_maillist = 'maillist.conf';
 $g_generator = 'generator.pl';
 $g_sendmail = 'sendmail.pl';
 $g_tmpdir = '/tmp/';
+$g_workers = 2;
--- tieba.pl
+++ tieba.pl
@@ -2,27 +2,62 @@
 
 use Cwd qw(abs_path);
 use File::Basename;
+use feature qw(say);
+use Time::HiRes qw(usleep);
 
 use open ':std', ':encoding(UTF-8)';
 
 
 $path = dirname(abs_path(__FILE__)).'/';
+
 require $path."config.pl";
+
 my $maillist = $g_maillist;
 my $generator = $g_generator;
 my $sendmail = $g_sendmail;
 my $tmpdir = $g_tmpdir;
+my $workers = $g_workers;
 
 open(my $FH, '<:encoding(UTF-8)', $path.$maillist) or die "failed open file.";
+
+my $counter = 0;
+my @pid = ($workers);
+
+#set to 1 to ensure parent process always do clean up steps,
+#in child process, fork() is always called and $curr_pid is always set to 0
+my $curr_pid = 1;
 
 while(<$FH>){
 	chomp;
 	my @array = split(/:/, $_);
 	my $keyword = $array[0];
 	my $file = $array[2];
-	system("cd $path&&scrapy crawl tieba -s FILENAME=$tmpdir$file -a keywords='$keyword'");
-	system("/usr/bin/env perl $path$generator $tmpdir$file > $tmpdir$file.html");
+
+	$counter++;
+	if($counter <= $workers){
+
+		$curr_pid = $pid[$counter - 1] = fork();
+		if($pid[$counter - 1] == 0){
+			system("from counter $counter, cd $path&&scrapy crawl tieba -s FILENAME=$tmpdir$file -a keywords='$keyword'");
+			system("from counter $counter, /usr/bin/env perl $path$generator $tmpdir$file > $tmpdir$file.html");
+			last;
+		}
+		else{
+			say("fork child done on counter = $counter, pid = $pid[$counter - 1]");
+		}
+	}
+	else{
+		foreach(@pid){
+			waitpid($_, WNOHANG);
+		}
+		$counter = 0;
+	}
 }
 
-close($FH);
-system("/usr/bin/env perl ".$path.$sendmail);
+if($curr_pid){
+	while(wait() >= 0){
+		usleep(100);
+	}
+	close($FH);
+	system("/usr/bin/env perl ".$path.$sendmail);
+}

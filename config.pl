8e3dde19ae1c7a10c64fd2bf3d171eed5e6e24aa
fozzy <fozzy@fozzy.co>
2017-02-23 10:33:59 +0800

    change worker processes to 12

--- config.pl
+++ config.pl
@@ -2,7 +2,7 @@ $g_maillist = 'maillist.conf';
 $g_generator = 'generator.pl';
 $g_sendmail = 'sendmail.pl';
 $g_tmpdir = '/tmp/';
-$g_workers = 6;
+$g_workers = 12;
 $g_sendgrid_enabled = 1;
 $g_gmail_enabled = 1;
 1;

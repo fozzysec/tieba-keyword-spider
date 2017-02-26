03afe659d00cfc9740fced9299abad5eebf2461e
fozzy <fozzy@fozzy.co>
2017-02-27 01:53:59 +0800

    fix bug to skip broken json

--- generator.pl
+++ generator.pl
@@ -5,6 +5,11 @@ use Cwd;
 use utf8;
 use File::stat;
 use Time::localtime;
+<<<<<<< HEAD
+=======
+use File::Basename;
+use Try::Tiny;
+>>>>>>> 080e79c... update generator.pl to skip broken json
 
 use open ':std', ':encoding(UTF-8)';
 
@@ -23,16 +28,20 @@ print("<table border='1'>\n");
 print("<tr><th>标题</th><th>作者</th><th>贴吧</th><th>预览</th><th>日期</th><th>地址</th></tr>\n");
 
 while(<$fh>){
-	chomp;
-	$json = JSON->new->utf8->decode($_);
-	print("<tr>\n");
-	print("<td>". $json->{'title'}.	"</td>\n");
-	print("<td>". $json->{'author'}.	"</td>\n");
-	print("<td>". $json->{'tieba'}.	"</td>\n");
-	print("<td>". $json->{'preview'}.	"</td>\n");
-	print("<td>", $json->{'date'}.	"</td>\n");
-	print("<td><a href=\"", $json->{'url'}.	"\" >link</a></td>\n");
-	print("</tr>\n");
+	try{
+		chomp;
+		$json = JSON->new->utf8->decode($_);
+		print("<tr>\n");
+		print("<td>". $json->{'title'}.	"</td>\n");
+		print("<td>". $json->{'author'}.	"</td>\n");
+		print("<td>". $json->{'tieba'}.	"</td>\n");
+		print("<td>". $json->{'preview'}.	"</td>\n");
+		print("<td>", $json->{'date'}.	"</td>\n");
+		print("<td><a href=\"", $json->{'url'}.	"\" >link</a></td>\n");
+		print("</tr>\n");
+	}catch{
+		next;
+	}
 }
 close($fh);
 print("</table></body></html>\n");
